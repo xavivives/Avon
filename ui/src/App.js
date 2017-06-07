e682b60846dac32eac7460c254a6992e73023151
Xavi <xavings@gmail.com>
2017-06-07 02:35:46 +0200

    Adding Creator component and top menu

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,41 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import VerticalNonLinear from './VerticalNonLinear';
-
+import Creator from './Creator';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 
+import AppBar from 'material-ui/AppBar';
+import CurrencyUtils from './CurrencyUtils';
+
+injectTapEventPlugin();
  var style = {
   marginRight: 20,
 }; 
 
-class App extends Component {
 
+/*
+<FloatingActionButton secondary={true} style={style}>
+                        <ContentAdd />
+                    </FloatingActionButton>*/
+
+
+
+
+class App extends Component {
    
   render() {
+
+    CurrencyUtils.getEtherPrice();
+
+    var  style = {
+      padding: 20
+    };
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
         <MuiThemeProvider>
             <div>
-            
-                <FloatingActionButton secondary={true} style={style}>
-                    <ContentAdd />
-                </FloatingActionButton>
 
-                <VerticalNonLinear/>
+                <AppBar title="Set a goal"iconClassNameRight="muidocs-icon-navigation-expand-more"/>
+
+                <div style = {style}>
+                    <Creator/>
+
+                    
+                </div>
 
             </div>
+
         </MuiThemeProvider>
       </div>
     );
